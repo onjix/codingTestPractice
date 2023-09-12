065509edad53004fb70599daf36e13b0e8bf2281
sonjix <101625609+onjix@users.noreply.github.com>
2023-09-12 14:27:48 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.09 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] solution(int[] arr) {
+        int[] answer = new int[arr.length-1];
+        if(arr.length == 1) {
+            arr[0] = -1;
+            return arr;
+        }
+        int min = arr[0];
+        for(int n : arr) {
+            if(min > n) {
+                min = n;
+            }
+        }
+        int index=0;
+        for(int i=0;i<arr.length;i++) {
+            if(arr[i] == min) {
+                continue;
+            }
+            answer[index++] = arr[i];
+        }
+        
+        return answer;
+    }
+}
