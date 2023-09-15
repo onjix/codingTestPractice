7fa664f61ed7ac564f7abde6410482b28348751e
sonjix <101625609+onjix@users.noreply.github.com>
2023-09-15 15:18:04 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 1.36 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] sArr = s.toLowerCase().split("");
+        boolean flag = true;
+        for(String str : sArr) {
+            answer += flag ? str.toUpperCase() : str;
+            flag = str.equals(" ") ? true : false;
+        }
+        return answer;
+    }
+}
