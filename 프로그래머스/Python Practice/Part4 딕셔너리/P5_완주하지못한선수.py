0010029f1c8b2fe7963f45df0c00179d530e2811
Jino Son <sojino0813@naver.com>
2024-01-22 16:36:15 +0900

    Revert "new file"
    This reverts commit bad026b222f53478dc5acde18975afdc01769d7a.

--- 프로그래머스/Python Practice/Part4 딕셔너리/P5_완주하지못한선수.py
+++
@@ -1,8 +0,0 @@
-from collections import Counter
-def solution(participant, completion):
-    answer = ''
-    cnt = Counter(participant) - Counter(completion)
-    for c in cnt:
-        answer += c
-
-    return answer
