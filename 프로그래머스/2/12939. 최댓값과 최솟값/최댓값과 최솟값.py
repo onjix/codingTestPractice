70d2dfe191bd714f2896b01bd759b20b698b4e79
sonjix <101625609+onjix@users.noreply.github.com>
2024-02-01 15:26:40 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.27 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -2,3 +2,5 @@ def solution(s):
     ar = s.split(' ')
     numlist = [eval(i) for i in ar]
     return str(min(numlist)) + ' ' + str(max(numlist))
+# map을 사용해서 하며 더 짧게 쓸수 있음
+# s = list(map(int, s.split()))
