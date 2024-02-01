d1626191721a34b638d71ef89f1d72304b16b917
sonjix <101625609+onjix@users.noreply.github.com>
2024-02-01 17:16:06 +0900

    [level 2] Title: 올바른 괄호, Time: 6.62 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,18 +1,12 @@
 def solution(s):
     answer = True
-    nl = list(s)
     cnt1 = 0
     cnt2 = 0
-    if nl[0] == ')':
-        return False
-    for i in nl:
+    for i in s:
         if '(' == i:
             cnt1 += 1
         else:
             cnt2 += 1
         if cnt1 < cnt2:
             return False
-    if cnt1 == cnt2:
-        return True
-    else:
-        return False
+    return cnt1 == cnt2
